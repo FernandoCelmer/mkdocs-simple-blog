8e25d122b78f1ecb65ddd76bab0503863ef32790
FernandoCelmer <email@fernandocelmer.com>
2023-01-18 00:23:18 -0300

    📌 ISSUE-#2 - Create dark mode and other color options

--- mkdocs_simple_blog/assets/js/theme.js
+++ mkdocs_simple_blog/assets/js/theme.js
@@ -24,4 +24,80 @@ document.addEventListener("DOMContentLoaded", function(){
         });
       })
     }
+
+    const themes = {
+      dark: {
+        '--text': 'white',
+        '--title': 'white',
+        '--primary': 'white',
+        '--background': 'black'
+      },
+      light: {
+        '--text': 'black',
+        '--title': 'black',
+        '--primary': 'black',
+        '--background': 'white'
+      }
+    };
+
+    [...document.querySelectorAll('.color-button')].forEach(el => {
+        el.addEventListener('click', () => {
+            const theme = themes[el.dataset.theme];
+            for (var variable in theme) {
+                document.documentElement.style.setProperty(variable, theme[variable]);
+            };
+        });
+    });
+
+    var buttons = document.querySelectorAll("button[color-primary]")
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function() {
+          var attr = this.getAttribute("color-primary")
+          document.documentElement.style.setProperty('--primary', attr);
+      })
+    })
+
+    var buttons = document.querySelectorAll("button[color-text]")
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function() {
+          var attr = this.getAttribute("color-text")
+          document.documentElement.style.setProperty('--text', attr);
+      })
+    })
+
+    var buttons = document.querySelectorAll("button[color-title]")
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function() {
+          var attr = this.getAttribute("color-title")
+          document.documentElement.style.setProperty('--title', attr);
+      })
+    })
+
+    var buttons = document.querySelectorAll("button[color-background]")
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function() {
+          var attr = this.getAttribute("color-background")
+          document.documentElement.style.setProperty('--background', attr);
+      })
+    })
+
+    var buttons = document.querySelectorAll("button[format-text]")
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function() {
+          var attr = this.getAttribute("format-text")
+          title = document.getElementById("title").classList;
+          title.remove("bold")
+          title.remove("italic")
+          title.remove("scratched")
+          title.remove("underline")
+          title.remove("overline")
+          title.add(attr);
+      })
+    })
+
+
+
+    
 });
+
+
