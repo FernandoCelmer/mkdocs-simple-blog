1305a45f82f0ddeb91496665ced5e431b4bf55d6
Fernando Celmer <email@fernandocelmer.com>
2025-01-13 05:32:41 -0300

    ⚙️ FEATURE-#42: Updated

--- setup.py
+++ setup.py
@@ -22,8 +22,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
