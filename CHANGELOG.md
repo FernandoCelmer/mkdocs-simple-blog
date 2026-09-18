e69f4f0dfddcd122fcb709919744fa6a0d5f881a
Action Platform <cloud@actionplatform.io>
2026-09-18 02:04:09 +0000

    chore(release): 0.6.2-rc.2

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## v0.6.2-rc.2 — 2026-09-18
+
+### Chores
+- **platform:** update configuration
--- mkdocs_simple_blog/__init__.py
+++ mkdocs_simple_blog/__init__.py
@@ -1,5 +1,5 @@
 """MkDocs Simple Blog theme package."""
 
-__version__ = "0.6.2-rc.1"
+__version__ = "0.6.2-rc.2"
 
 __author__ = "Fernando Celmer <email@fernandocelmer.com>"
